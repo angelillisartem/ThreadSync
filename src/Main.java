b5dd857656f45a646a87500c18754d87c5e4d222
angelillis.artem <angelillis.artem@studenti.salesianisesto.it>
2023-03-17 13:18:32 +0100

    Proseguimento 1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,39 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        MyThread t1 = new MyThread();
-//        t1.start();
-//        MyThinThread mt1 = new MyThinThread();
-//        Thread t2 = new Thread(mt1);
-//        t2.start();
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.print("\nCiao sono sottile a runnable e sono " + Thread.currentThread().getName());
-//            }
-//        }).start();
-//
-//        ContoCorrente cc = new ContoCorrente(1000);
-//        Thread gen1 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                cc.versa(1000);
-//                cc.stampaSaldo();
-//            }
-//        });
-//        Thread gen2 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                cc.versa(500);
-//                cc.stampaSaldo();
-//            }
-//        });
-//        gen1.start();
-//        gen2.start();
+        //        Inizio Commento
+        //        MyThread t1 = new MyThread();
+        //        t1.start();
+        //        MyThinThread mt1 = new MyThinThread();
+        //        Thread t2 = new Thread(mt1);
+        //        t2.start();
+        //        new Thread(new Runnable() {
+        //            @Override
+        //            public void run() {
+        //                System.out.print("\nCiao sono sottile a runnable e sono: " + Thread.currentThread().getName());
+        //            }
+        //        }).start();
+        //
+        //        ContoCorrente cc = new ContoCorrente(1000);
+        //        Thread gen1 = new Thread(new Runnable() {
+        //            @Override
+        //            public void run() {
+        //                cc.versa(1000);
+        //                cc.stampaSaldo();
+        //            }
+        //        });
+        //        Thread gen2 = new Thread(new Runnable() {
+        //            @Override
+        //            public void run() {
+        //                cc.versa(500);
+        //                cc.stampaSaldo();
+        //            }
+        //        });
+        //        gen1.start();
+        //        gen2.start();
+        //        Fine Commento
 
         Incrementatore incrementatore = new Incrementatore();
-       Thread t1 = new Thread(new Runnable() {
+        Thread t1 = new Thread(new Runnable() {
            @Override
            public void run() {
                for (int i = 0; i < 10000; i++) {
@@ -39,12 +41,20 @@ public class Main {
                }
            }
        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incrementatore.incrementa();
+                }
+            }
+        });
+
        t1.start();
-       try {
-           Thread.sleep(1000);
-       } catch {
-           throw new RuntimeException();
-        }
+       t2.start();
+       t1.join();
+       t2.join();
        System.out.print("Incrementatore: " + incrementatore.incrementometro);
     }
+
 }
